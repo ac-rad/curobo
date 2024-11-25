62e3021e9f6da3c0eee79c9d9530a16543e7fea9
yijie-04 <yijiewang0806@gmail.com>
2024-11-25 08:32:51 -0500

    Revert "motion gen"
    This reverts commit 8f7b2345aef01b7980697d3ae1c09243f0eabbb7.

--- src/curobo/wrap/reacher/motion_gen.py
+++ src/curobo/wrap/reacher/motion_gen.py
@@ -83,7 +83,6 @@ from curobo.wrap.reacher.ik_solver import IKResult, IKSolver, IKSolverConfig
 from curobo.wrap.reacher.trajopt import TrajOptResult, TrajOptSolver, TrajOptSolverConfig
 from curobo.wrap.reacher.types import ReacherSolveState, ReacherSolveType
 
-
 @dataclass
 class MotionGenConfig:
     """Configuration dataclass for creating a motion generation instance."""
